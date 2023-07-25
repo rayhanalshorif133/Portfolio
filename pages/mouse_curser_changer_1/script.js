0c20ec6d0b70875d9192cc4a1ca859f38e68a9c9
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-07-25 10:45:02 +0600

    curser style sidebar added

--- pages/mouse_curser_changer_1/script.js
+++ pages/mouse_curser_changer_1/script.js
@@ -2,21 +2,48 @@ $(function(){
     $(window).mousemove(function(e){
         var x = e.pageX;
         var y = e.pageY;
-        // move the pointer-ring
-
-
-        setTimeout(function(){
-            $('#pointer-ring').css({
-                'left': x,
-                'top': y, 
-            });
-        }, 100);
-
-        // move the pointer
-        $('#pointer-dot').css({
-            'left': x,
-            'top': y,
-        });
-
+        handlePointerRingCursor(x,y);
     });
+    handleSideBarSettings();
 });
+
+const handlePointerRingCursor = (x,y) => {
+    setTimeout(function(){
+        $('#pointer-ring').css({
+            'left': x,
+            'top': y, 
+        });
+    }, 100);
+    // move the pointer
+    $('#pointer-dot').css({
+        'left': x,
+        'top': y,
+    });
+}
+
+const handleSideBarSettings = () => {
+    $(".sidebar_settings").click(function(){
+        $(".sidebar").toggleClass("active");
+        if($(".sidebar").hasClass("active")){
+            var sidebarWidth = 0;
+            setInterval(function(){
+                if(sidebarWidth < 16){
+                    sidebarWidth = sidebarWidth + 1;
+                    $(".sidebar").css('width', sidebarWidth + "%");
+                }
+            }, 30);
+            $(".sidebar .content").toggleClass('hidden');
+        }else{
+            var sidebarWidth = 15;
+            setInterval(function(){
+                if(sidebarWidth != 0){
+                    sidebarWidth = sidebarWidth - 1;
+                    $(".sidebar").css('width', sidebarWidth + "%");
+                }
+            }, 30);
+            setTimeout(function(){
+                $(".sidebar .content").toggleClass('hidden');
+            }, 450);
+        }
+    });
+};
