a23d169def8760410b92eccef47b7b68c5cf0555
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-07-25 16:29:09 +0600

    working on protfolio

---
+++ pages/portfolio_1/js/script.js
@@ -0,0 +1,41 @@
+$(function () {
+    handleBgGradientToR();
+    handleTypeWriter();
+});
+
+const handleBgGradientToR = () => {
+    const bgGradientFromAndTo = ['from-sky-500 to-indigo-500','from-cyan-500 to-blue-500','from-violet-500 to-fuchsia-500','from-purple-500 to-pink-500']
+    setInterval(() => {
+        const random = Math.floor(Math.random() * bgGradientFromAndTo.length);
+        const bgGradient = bgGradientFromAndTo[random];
+        var from = $(".largeProfileImage").attr('class').split(' ').find(c => c.startsWith('from'));
+        var to = $(".largeProfileImage").attr('class').split(' ').find(c => c.startsWith('to'));
+        $(".largeProfileImage").removeClass(from).removeClass(to);
+        $(".largeProfileImage").addClass(bgGradient);
+    }, 1000);
+}
+
+const handleTypeWriter = () => {
+
+    var app = document.getElementById('designationTypeWriter');
+    
+    var typewriter = new Typewriter(app, {
+        loop: true,
+        typingSpeed: 100,
+        deleteSpeed: 100,
+        delay: 100,
+        cursor: '|',
+        strings: ['Full Stack Developer', 'Professional Coder']
+        
+    });
+    
+    typewriter.typeString('Full Stack Developer')
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString('Professional Coder')
+        .pauseFor(2500)
+        .start();
+
+
+}
+
---
+++ pages/portfolio_1/js/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        screens: {
+            'lg': '1021px',
+        },
+        fontFamily: {
+            'montserrat': ['Montserrat', 'sans-serif'],
+            'poppins': ['Poppins', 'sans-serif'],
+        },
+        extend: {
+             colors: {
+                 primary: '#ff014f',
+                 primarySub: '#f9004d',
+                 facebook: '#1877F2',
+                 twitter: '#1da1f2',
+                 email: '#EA4335',
+             }
+        }
+    }
+}
