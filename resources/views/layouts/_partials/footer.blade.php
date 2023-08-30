04eb94e45dd3b6f725e49d75efe332d149f358c8
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-08-30 15:14:06 +0600

    working on dashboard

---
+++ resources/views/layouts/_partials/footer.blade.php
@@ -0,0 +1,21 @@
+<div
+class="px-6 pt-2 pb-6 app-sidebar-footer flex-column-auto"
+id="kt_app_sidebar_footer">
+<a
+    href="{{route('welcome')}}"
+    class="px-0 overflow-hidden btn btn-flex flex-center btn-custom btn-primary text-nowrap h-40px w-100"
+    data-bs-toggle="tooltip"
+    data-bs-trigger="hover"
+    data-bs-dismiss-="click"
+    target="_blank"
+    title="
+    Protfolio View
+    ">
+    <span class="btn-label">
+        Home Page
+    </span>
+    <i
+        class="m-0 ki-duotone ki-document btn-icon fs-2"><span
+            class="path1"></span><span class="path2"></span></i>
+</a>
+</div>
